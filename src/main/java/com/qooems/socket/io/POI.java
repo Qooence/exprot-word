c8a2be00bbde014f2a7e83c5a2d0f2577a9fe13f
Qooems <1044212759@qq.com>
2020-07-14 23:53:25 +0800

    dd

--- src/main/java/com/qooems/socket/io/POI.java
+++ src/main/java/com/qooems/socket/io/POI.java
@@ -1,15 +1,26 @@
 package com.qooems.socket.io;
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
 import cn.afterturn.easypoi.word.WordExportUtil;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
+import java.io.File;
 import java.io.FileOutputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class POI {
 
     public static void main(String[] args) {
+        POI poi = new POI();
+        poi.simpleWrite();
+    }
+
+    public void export_tm(){
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("name", " ");
         map.put("age", "45");
@@ -21,11 +32,83 @@ public class POI {
         try {
             XWPFDocument doc = WordExportUtil.exportWord07(
                     "doc/test.docx", map);
-            FileOutputStream fos = new FileOutputStream("D://image.docx");
+            FileOutputStream fos = new FileOutputStream("E://image.docx");
             doc.write(fos);
             fos.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void fe_map() throws Exception {
+        TemplateExportParams params = new TemplateExportParams(
+                "doc/xxx.xls");
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("price", 10000);
+        map.put("money", 2000000.00);
+        map.put("upperMoney", "贰佰万");
+        map.put("company", "执笔潜行科技有限公司");
+        map.put("bureau", "财政局");
+        map.put("person", "JueYue");
+        map.put("phone", "1879740****");
+        List<Map<String, String>> listMap = new ArrayList<Map<String, String>>();
+        for (int i = 0; i < 4; i++) {
+            Map<String, String> lm = new HashMap<String, String>();
+            lm.put("id", i + 1 + "");
+            lm.put("zijin", i * 10000 + "");
+            lm.put("bianma", "A001");
+            lm.put("mingcheng", "设计");
+            lm.put("xiangmumingcheng", "EasyPoi " + i + "期");
+            lm.put("quancheng", "开源项目");
+            lm.put("sqje", i * 10000 + "");
+            lm.put("hdje", i * 10000 + "");
+
+            listMap.add(lm);
+        }
+        map.put("maplist", listMap);
+
+        Workbook workbook = ExcelExportUtil.exportExcel(params, map);
+        File savefile = new File("E:/excel/");
+        if (!savefile.exists()) {
+            savefile.mkdirs();
+        }
+        FileOutputStream fos = new FileOutputStream("E:/excel/专项支出用款申请书_map.xls");
+        workbook.write(fos);
+        fos.close();
+    }
+
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<DemoData>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+    public void simpleWrite() {
+        // 写法1
+        String fileName = TestFileUtil.getPath() + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+
+        // 写法2
+        fileName = TestFileUtil.getPath() + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = null;
+        try {
+            excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+            WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+            excelWriter.write(data(), writeSheet);
+        } finally {
+            // 千万别忘记finish 会帮忙关闭流
+            if (excelWriter != null) {
+                excelWriter.finish();
+            }
         }
     }
 }
